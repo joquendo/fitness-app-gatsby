0f9e079be928a8cb690cddf166915ce0ee858f2e
James Oquendo <james.oquendo@gmail.com>
2020-04-21 10:31:52 -0700

    Fitness webapp using Gatsby

---
+++ fitness-app-gatsby/src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new fitness source.</p>
+    <p>Let me keep you in your healthiest body for the rest of your life.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
