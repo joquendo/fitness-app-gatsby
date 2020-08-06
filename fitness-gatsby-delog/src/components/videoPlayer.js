7d2f102ddac7ad5ac6614af67bab68c2e7d89357
James Oquendo <james.oquendo@gmail.com>
2020-08-05 23:41:14 -0700

    Create a video player and playlist using the HTML video element and gatsby (WIP)

--- fitness-gatsby-delog/src/components/videoPlayer.js
+++ fitness-gatsby-delog/src/components/videoPlayer.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import fitnessVideo from "../../static/assets/vllo.mp4"
+import React, { Component } from 'react'
+import playlistVideos from '../../content/playlist.json'
 
-export default function VideoPlayer() {
-    const videoStyles = {
-        width: '100%'
+
+
+// Playlist subcomponent
+const Playlist = (props) => {
+
+  return (
+    <div>
+        <ul>
+          {playlistVideos.map((data, index) => {
+            return <li key={`video_${index}`} onClick={props.playSelectedVideo} data-index={index}>{data.title}</li>
+          })}
+        </ul>
+      </div>
+  )
+        }
+
+class VideoPlayer extends Component {
+
+    state = {
+      //'playingVideo': false
     };
 
-    return (
-      <video controls autoPlay muted style={videoStyles}>
-        <source src={fitnessVideo} type="video/mp4" />
-      </video>
-    );
+    videoStyles = {
+      width: '100%'
+    };
+
+    playSelectedVideo = (e) => {
+      console.log(e.target.dataset.index);
+
+      const videoPlayer = this.refs.vidRef;
+      videoPlayer.pause()
+
+      videoPlayer.src = `../assets/${playlistVideos[e.target.dataset.index].source}.mp4`;
+    }
+
+    render () {
+      return (
+        <div>
+          <video id="vid" ref="vidRef" src={`../assets/${playlistVideos[0].source}.mp4`} controls autoPlay muted style={this.videoStyles} />
+          <Playlist playSelectedVideo={this.playSelectedVideo}/>
+        </div>
+      )
+    }
   }
+
+  export default VideoPlayer;
